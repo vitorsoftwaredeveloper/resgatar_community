0a7e727d2fddc44a4793aa3583055b2af82a79fe
vitorsoaresferreira <vitorsoftwaredeveloper@gmail.com>
2026-01-24 09:15:02 -0300

    feat: implements list nofifications only thirty days ago until now

--- src/services/notifications/listNotifications.ts
+++ src/services/notifications/listNotifications.ts
@@ -1,16 +1,24 @@
 import { INotification } from "../../types/notification";
 import { NotificationModel } from "../../models/Notification";
+import { create } from "domain";
 
 export const listNotificationService = async (): Promise<
   Array<INotification>
 > => {
   console.log("IN - listNotificationService");
 
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+
   try {
     const notifications: Array<INotification> = await NotificationModel.find(
+      {
+        createdAt: {
+          $gte: thirtyDaysAgo,
+          $lte: new Date(),
+        },
+      },
       {},
-      {},
-      { sort: { createdAt: -1 }, lean: true }
+      { sort: { createdAt: -1 }, lean: true },
     );
 
     const TEN_DAYS_IN_MS = 10 * 24 * 60 * 60 * 1000;
