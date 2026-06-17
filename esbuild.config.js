9979b2eebf988078d36f353e8233587be4c308bc
vitorsoaresferreira <vitorsoftwaredeveloper@gmail.com>
2026-06-16 21:12:14 -0300

    chore: update config serverless functions

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   bundle: true,
   minify: true,
-  sourcemap: false, // remove mapas em prod
   platform: "node",
-  target: "node24", // Mantém dependências da AWS fora do bundle
-  external: ["aws-sdk"], // Mantém dependências da AWS fora do bundle
-  loader: {
-    ".json": "json", // Inclui arquivos JSON automaticamente no bundle
-  },
-  sourcesContent: false, // Para builds mais rápidos
-  keepNames: false, // permite minificação completa
+  target: "node24",
+  concurrency: 10,
 };
